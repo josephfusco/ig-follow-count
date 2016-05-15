67604651c0abbbae938e968151a0fa1852d7179a
Joseph Fusco <hello@josephfus.co>
2016-05-15 01:50:07 -0400

    Fix some bootstrap conflicting styles.

--- includes/header.php
+++ includes/header.php
@@ -14,6 +14,8 @@ if ( ! defined( 'ABSPATH' ) ) exit ( "Direct access not permitted." );
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="icon" type="image/png" sizes="150x150" href="<?php echo get_data( 'profile_picture' ); ?>">
      <link rel="apple-touch-icon" sizes="150x150" href="<?php echo get_data( 'profile_picture' ); ?>">
+     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+     
      <?php load_theme_styles(); ?>
 
      <script type="text/javascript" src="js/jquery.min.js"></script>
