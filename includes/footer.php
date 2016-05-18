74b94fdfd9493e0130c9b1803e3b3fc51f82f4f2
Joseph Fusco <hello@josephfus.co>
2016-05-18 17:03:20 -0400

    Clean up.

--- includes/footer.php
+++ includes/footer.php
@@ -7,10 +7,6 @@ if ( ! defined( 'ABSPATH' ) ) exit ( "Direct access not permitted." );
 
 </main>
 
-<footer>
-    <a href="https://github.com/josephfusco/ig-follow-count" target="_blank">GitHub</a>
-</footer>
-
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/app.js"></script>
 
--- includes/header.php
+++ includes/header.php
@@ -14,6 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) exit ( "Direct access not permitted." );
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="icon" type="image/png" sizes="150x150" href="<?php echo get_data( 'profile_picture' ); ?>">
      <link rel="apple-touch-icon" sizes="150x150" href="<?php echo get_data( 'profile_picture' ); ?>">
+     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
 
      <?php load_theme_styles(); ?>
 
@@ -28,11 +29,14 @@ if ( ! defined( 'ABSPATH' ) ) exit ( "Direct access not permitted." );
             <summary><span><?php echo get_count( 'media' ); ?></span> posts <span><?php echo get_count( 'followed_by' ); ?></span> followers <span><?php echo get_count( 'follows' ); ?></span> following</summary>
         </div>
 
-        <!--
         <ul class="nav">
-            <li class="<?= (get_active_page() == 'dashboard') ? 'active':''; ?>"><a href="/">Dashboard</a></li>
+            <li>
+                <a href="<?php echo get_log_file(); ?>" download><i class="fa fa-download"></i><span>Download CSV</span></a>
+            </li>
+            <li>
+                <a href="https://github.com/josephfusco/ig-follow-count" target="_blank"><i class="fa fa-github"></i><span>GitHub</span></a>
+            <li>
         </ul>
-        -->
 
     </nav>
 
